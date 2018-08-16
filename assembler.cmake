94c9685dccadb50b6c37dca17858b45a74db85f9
Richard Buckley <richard@hrbuckley.net>
2018-08-16 18:12:56 -0400

    Refactor assembler.cmake

--- assembler.cmake
+++ assembler.cmake
@@ -1,18 +1,11 @@
-set(ASSEMBLER_ROOT src/pdp8/assembler)
-set(PARSER_ROOT src/pdp8/asm_parser)
-
 include_directories(
-        ${PROJECT_SOURCE_DIR}/${ASSEMBLER_ROOT}
-        ${PROJECT_SOURCE_DIR}/${PARSER_ROOT}
+        src/pdp8/assembler
+        src/pdp8/asm_parser
 )
 
 file(GLOB ASSEMBLER_SOURCES
-        ${PROJECT_SOURCE_DIR}/${ASSEMBLER_ROOT}/*.cpp
-        ${PROJECT_SOURCE_DIR}/${PARSER_ROOT}/*.cpp
+        src/pdp8/assembler/*.cpp
+        src/pdp8/asm_parser/*.cpp
         )
-
-foreach (_source ${ASSEMBLER_SOURCES})
-    set_source_files_properties(${_source} PROPERTIES GENERATED 1)
-endforeach ()
 
 add_library(assembler ${ASSEMBLER_SOURCES})
